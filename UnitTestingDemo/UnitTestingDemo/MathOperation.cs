946fee67c619689387ab5a2c992b59be89841295
sanjay.kumar <sanjay.kumar@taazaa.com>
2023-08-11 20:57:34 +0530

    Unit test basis 3

---
+++ UnitTestingDemo/UnitTestingDemo/MathOperation.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+public static class MathOperation
+{
+    public static double Add(double number1, double number2)
+    {
+        return (number1 + number2);
+    }
+
+    public static double Subtract(double number1, double number2)
+    {
+        return (number1 - number2);
+    }
+
+    public static double Multiply(double number1, double number2)
+    {
+        return (number1 * number2);
+    }
+
+    public static double Divide(double number1, double number2)
+    {
+        return (number1 / number2);
+    }
+}
